ad532673a136efb3b96b0f597229773b798e3058
egorkaMegaMozg228 <zemtsov.yehor@gmail.com>
2024-10-30 08:27:33 +0200

    Add files via upload

---
+++ Lab8.1_rec/UnitTest8.1_rec/UnitTest8.1_rec.cpp
@@ -0,0 +1,27 @@
+﻿
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab8.1_rec.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest81rec
+{
+	TEST_CLASS(UnitTest81rec)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			char* dest1 = new char[151];
+			dest1[0] = ',';
+			dest1[1] = '-';
+			dest1[2] = '\0';
+				
+			char expectedCount = 1;
+
+			char actualCount = Count(dest1,0);
+			Assert::AreEqual(actualCount, expectedCount);
+		}
+	};
+}
